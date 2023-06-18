2a9627436a776a04bc7548774486ba613bcce85a
Calebe Navarro <calebe.navarro@spro.com.br>
2023-06-18 11:16:17 -0300

    fix: correct gitignore created

---
+++ src/teachercalculator/TeacherCLT.java
@@ -0,0 +1,11 @@
+package teachercalculator;
+
+public class TeacherCLT extends BaseTeacher {
+	
+	public TeacherCLT(String name, double salary)
+	{
+		super(name);
+		this.setSalary(salary);
+	}
+	
+}
